61e5cd8746dc7bb89c466fdfdf4888fc7c45d02e
Mario Villacreses <MarioVillacreses@outlook.com>
2018-08-11 16:02:07 -0400

    Move project loading to the top of the app

--- client/components/dashboard/ProjectList.js
+++ client/components/dashboard/ProjectList.js
@@ -2,14 +2,12 @@ import React from 'react'
 import {connect} from 'react-redux'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
+import Button from '@material-ui/core/Button'
 import AddProject from './AddProject'
 import {fetchProjects} from '../../store'
 import {Link} from 'react-router-dom'
 
 class ProjectList extends React.Component {
-  componentDidMount() {
-    this.props.loadProjects()
-  }
 
   render() {
     const {projects, handleClick} = this.props
@@ -18,7 +16,6 @@ class ProjectList extends React.Component {
         {projects.map(project => (
           <ListItem
             key={project.id}
-            button
           >
             <Link to={`/d/project#${project.id}`}>{project.title}</Link>
         </ListItem>
@@ -35,9 +32,5 @@ const mapStateToProps = state => ({
 projects: Object.values(state.projects),
 })
 
-// to do: replace console.log with a thunk for fetching lists associated with the selected project
-const mapDispatchToProps = dispatch => ({
-loadProjects: () => dispatch(fetchProjects()),
-})
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProjectList)
+export default connect(mapStateToProps)(ProjectList)
